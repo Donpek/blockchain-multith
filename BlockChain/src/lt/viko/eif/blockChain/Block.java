dbe2b3cefa1d193e3fe61bc537bcd333f85f5012
Ivan Stanul <gitlab@ivanstanul.com>
2019-05-30 11:59:15 +0300

    sample block

--- BlockChain/src/lt/viko/eif/blockChain/Block.java
+++ BlockChain/src/lt/viko/eif/blockChain/Block.java
@@ -1,5 +1,17 @@
 package lt.viko.eif.blockChain;
 
-public class Block {
+import java.util.Date;
 
+public class Block {
+  public String hash;
+  public String previousHash;
+  private String data; //Something data...
+  private long timeStamp; //as number of milliseconds since 1/1/1970.
+
+  //Block Constructor.
+  public Block(String data,String previousHash ) {
+    this.data = data;
+    this.previousHash = previousHash;
+    this.timeStamp = new Date().getTime();
+  }
 }
