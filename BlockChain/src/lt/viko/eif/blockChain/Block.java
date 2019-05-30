d7713e0eb48016aa034258f75445d21d590ab139
Eimantas Ilkevicius <eimantas1998@gmail.com>
2019-05-30 12:20:17 +0300

    Getters setters

--- BlockChain/src/lt/viko/eif/blockChain/Block.java
+++ BlockChain/src/lt/viko/eif/blockChain/Block.java
@@ -7,11 +7,70 @@ public class Block {
   public String previousHash;
   private String data; //Something data...
   private long timeStamp; //as number of milliseconds since 1/1/1970.
+  
+  
 
-  //Block Constructor.
+  public String getHash() {
+	return hash;
+}
+
+
+
+public void setHash(String hash) {
+	this.hash = hash;
+}
+
+
+
+public String getPreviousHash() {
+	return previousHash;
+}
+
+
+
+public void setPreviousHash(String previousHash) {
+	this.previousHash = previousHash;
+}
+
+
+
+public String getData() {
+	return data;
+}
+
+
+
+public void setData(String data) {
+	this.data = data;
+}
+
+
+
+public long getTimeStamp() {
+	return timeStamp;
+}
+
+
+
+public void setTimeStamp(long timeStamp) {
+	this.timeStamp = timeStamp;
+}
+
+
+
+//Block Constructor.
   public Block(String data,String previousHash ) {
     this.data = data;
     this.previousHash = previousHash;
     this.timeStamp = new Date().getTime();
   }
+
+
+
+@Override
+public String toString() {
+	return "Block [hash=" + hash + ", previousHash=" + previousHash + ", data=" + data + ", timeStamp=" + timeStamp
+			+ "]";
+}
+   
 }
